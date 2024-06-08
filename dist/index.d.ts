16e6556c8105ace12e2c77959b22637e424c064a
bladerunnaer2020 <pivovarov@gmail.com>
2024-06-08 19:28:11 +0300

    Removed dist and vscode from repository

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const toBoolean: (s?: boolean | number | string | null) => boolean;
-export type TimeParser = {
-    re: RegExp;
-    f: (res: string[]) => number;
-};
-export declare const toMilliseconds: (s?: string | number) => number | null;
-export declare const setTimeParsers: (parsers: TimeParser[]) => void;
-export declare const toNumber: (s?: string | boolean | number) => number | null;
-export declare const fromMsToString: (ms?: number, format?: string, precision?: number) => string | null;
